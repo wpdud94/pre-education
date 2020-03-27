c15351680553a0601e8b798137950798e64ed250
강제영 <wpdud001@gmail.com>
2020-03-27 19:10:16 +0900

    Answer quiz1 02

--- quiz/pre_python_02.py
+++ quiz/pre_python_02.py
@@ -10,3 +10,24 @@
 <출력>
 150
 """
+print('첫 번째 수를 입력하세요 :', end = '')
+input_f = float(input())
+print('두 번째 수를 입력하세요 :', end = '')
+input_s = float(input())
+print('어떤 연산을 하실 건가요? :', end = '')
+input_c = input()
+
+if input_c == '+':
+    answer = input_f + input_s
+    print(answer)
+elif input_c == '-':
+    answer = input_f - input_s
+    print(answer)
+elif input_c == '*':
+    answer = input_f * input_s
+    print(answer)
+elif input_c == '/':
+    answer = input_f / input_s
+    print(answer)
+else :
+    print('잘못된 연산입니다.')
